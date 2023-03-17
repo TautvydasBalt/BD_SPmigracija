dceceff192ad70dd3bc2c9555d2587dd056078da
Tautvydas Baltaduonis <tautvydas.baltaduonis@stud.vilniustech.lt>
2023-03-17 13:59:39 +0200

    request page connect to backend

--- spmm-app/src/Pages/MigrationRequestsPage/MigrationRequestsPage.tsx
+++ spmm-app/src/Pages/MigrationRequestsPage/MigrationRequestsPage.tsx
@@ -1,37 +1,53 @@
-import { GroupedList } from '@fluentui/react';
+import { List } from '@fluentui/react';
+import axios from 'axios';
 import React from 'react';
 import Navbar from '../../components/NavBar/NavBar';
 import styles from './MigrationRequestsPage.module.scss';
 
-class MigrationRequestsPage extends React.Component {
-  render() {
+class MigrationRequestsPage extends React.Component<{}, { migrationRequests: any }> {
+
+  constructor(props: {}) {
+    super(props);
+    
+    this.state = {
+      migrationRequests: [],
+    }
+    this.getMigrationRequests = this.getMigrationRequests.bind(this);
+  }
+
+  public componentDidMount(): void {
+    this.getMigrationRequests();
+  }
+
+  public render() {
+    const { migrationRequests } = this.state;
     return (
       <div>
         <Navbar />
         <div className={styles.container}>
           SPMigration
-          <GroupedList onRenderCell={this.onRenderCell} items={mock_items} />
+          <List onRenderCell={this.onRenderCell} items={migrationRequests} />
         </div>
       </div>
     );
   }
 
-  private onRenderCell = (nestingDepth?: number, item?: any, itemIndex?: number): React.ReactNode => {
+  private onRenderCell = (item?: any, index?: number, isScrolling?: boolean): React.ReactNode => {
     return item ? (
-      <div className={styles.card} data-selection-index={itemIndex}>
-          {item.assingedTo}
-
-          {item.source}
+      <div className={styles.itemCell} data-is-focusable={true}>
+      <div className={styles.itemContent}>
+        <div className={styles.itemName}>{item.requestName}</div>
+        <div className={styles.itemData}>{"Source URL: " + item.sourceURL}</div>
+        <div className={styles.itemData}>{"Destination URL: " +item.destinationURL}</div>
       </div>
+    </div>
     ) : null;
   };
 
+  private async getMigrationRequests() {
+    const response = await axios.get(`/allRequests`);
+    this.setState({migrationRequests: response.data})
+  }
 }
-
-const mock_items = [
-  { key: 0, assingedTo: "Worker 1", source: "URL", destination: "URL", status: "Active" },
-  { key: 1, assingedTo: "Worker 2", source: "URL", destination: "URL", status: "Aproved" },
-]
-
 
 export default MigrationRequestsPage;
